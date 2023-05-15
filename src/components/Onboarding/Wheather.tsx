e738c2c09a4648cf48fb12d1bb78f49b476f6166
Mammadli17 <isamammadli17@gmail.com>
2023-05-16 00:24:49 +0400

    salam

--- src/components/Onboarding/Wheather.tsx
+++ src/components/Onboarding/Wheather.tsx
@@ -6,12 +6,10 @@ import Geocoder from 'react-native-geocoding';
 import axios from 'axios';
 import { Loc } from '../images';
 
-<<<<<<< HEAD
+
 
 const API_KEY = '729cf7aad0a46677bbf6c8da49f416b5'; // Replace 'YOUR_API_KEY' with your OpenWeatherMap API key
-=======
-const API_KEY = 'daac4b59547b857ad8e0869083c4d642';  // Replace 'YOUR_API_KEY' with your Google Maps Geocoding API key
->>>>>>> cf0bc52bbd66d6890360a7d30be532789dc5cccd
+
 
 const App = () => {
   const [latitude, setLatitude] = useState<any>(null);
@@ -57,9 +55,6 @@ const App = () => {
     .catch((error) => {
       console.log(error);
     });
-<<<<<<< HEAD
-
-=======
     axios.get(`https:// /data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`)
     .then(response => {
       setWeatherData(response.data.main.temp)
@@ -68,8 +63,7 @@ const App = () => {
     .catch(error => {
       console.log(error);
     });
-  
->>>>>>> cf0bc52bbd66d6890360a7d30be532789dc5cccd
+
     
     axios
       .get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`)
