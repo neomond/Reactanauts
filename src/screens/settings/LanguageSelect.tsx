a7a7a753fdbdb8d9421f6c4ccdb09bb141bb2250
neomond <neoweise@yahoo.com>
2023-05-19 23:31:23 +0400

    changed settings UI

--- src/screens/settings/LanguageSelect.tsx
+++ src/screens/settings/LanguageSelect.tsx
@@ -1,44 +1,88 @@
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
-import React, { useState } from 'react'
-import '../../locales/i18n'
-import { useTranslation } from "react-i18next";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  SafeAreaView,
+} from 'react-native';
+import React, {useState} from 'react';
+import '../../locales/i18n';
+import {useTranslation} from 'react-i18next';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-const LanguageSelect = ({ navigation }: any) => {
 
+const LanguageSelect = ({navigation}: any) => {
+  const [currentLanguage, setcurrentLanguage] = useState('az');
 
+  const {t, i18n} = useTranslation();
 
+  const changeLang = (lang: string) => {
+    i18n.changeLanguage(lang).then(() => {
+      // this.props.close();
+      i18n.options.lng = lang;
+      AsyncStorage.setItem('language', lang);
+      setcurrentLanguage(lang);
+    });
+  };
 
-    const [currentLanguage, setcurrentLanguage] = useState('az')
-
-    const { t, i18n } = useTranslation();
-
-    const changeLang = (lang: string) => {
-
-        i18n.changeLanguage(lang).then(() => {
-            // this.props.close();
-            i18n.options.lng = lang;
-            AsyncStorage.setItem('language', lang);
-            setcurrentLanguage(lang)
-        });
-
-
-
-    }
-
-    return (
-        <View>
-            <Text style={{marginLeft: 125, fontSize:24, fontWeight:'700',color:'black'}}>{t("selectlang")}</Text>
-
-            <TouchableOpacity onPress={() => {changeLang('az');navigation.navigate("SettingsMain")}}>
-                <Text style={{fontSize:20,color:'green'}}>Azərbaycanca</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => {changeLang('en');navigation.navigate("SettingsMain")}}>
-                <Text style={{fontSize:20,color:'green'}}>English</Text>
-            </TouchableOpacity>
+  return (
+    <SafeAreaView style={styles.main}>
+      <Text style={styles.header}>{t('selectlang')}</Text>
+      <View style={styles.langTypesWrapper}>
+        <View style={styles.langItems}>
+          <Text style={styles.langFlag}>🇦🇿</Text>
+          <TouchableOpacity
+            onPress={() => {
+              changeLang('az');
+              navigation.navigate('SettingsMain');
+            }}>
+            <Text style={styles.langText}>Azərbaycanca</Text>
+          </TouchableOpacity>
         </View>
-    )
-}
+        <View style={styles.langItems}>
+          <Text style={styles.langFlag}>🇬🇧</Text>
+          <TouchableOpacity
+            onPress={() => {
+              changeLang('en');
+              navigation.navigate('SettingsMain');
+            }}>
+            <Text style={styles.langText}>English</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
 
-export default LanguageSelect
+export default LanguageSelect;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    backgroundColor: '#1c1c1c',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header: {
+    textAlign: 'center',
+    fontSize: 24,
+    fontWeight: '700',
+    color: '#fff',
+    marginBottom: 15,
+  },
+  langTypesWrapper: {
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+  },
+  langText: {
+    fontSize: 20,
+    color: '#fff',
+  },
+  langItems: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    columnGap: 12,
+  },
+  langFlag: {
+    fontSize: 22,
+  },
+});
